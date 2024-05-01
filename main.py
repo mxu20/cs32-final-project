7a4f8de0a878215757370e57a143051ad98db501
mxu20 <144145993+mxu20@users.noreply.github.com>
2024-04-30 21:45:53 -0400

    Finished main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 import requests
 import pandas as pd
 import time
@@ -12,129 +12,183 @@ from email.mime.text import MIMEText
 import schedule
 import json
 
-# Create a dictionary 
-URL = [
-    ("https://www.aljazeera.com/", "Al Jazeera"),
-    ("https://www.bbc.com/news", "BBC"),
-    ("https://www.reuters.com/", "Reuters"),
-    ("https://www.scmp.com/", "SCMP"),
-    ("https://apnews.com/world-news", "AP News")
-       ]
+# Create a dictionary of news sites
+URL_list = ['https://www.bbc.com/news', 
+            'https://www.aljazeera.com/', 
+            'https://www.reuters.com/', 
+            'https://www.scmp.com/', 
+            'https://apnews.com/world-news']
 
-news = []
-# HTTP Get request to website
-for url, source in URL:
-    r = requests.get(url)
-    # print(r.content)
-
-# Parse content
-    soup = BeautifulSoup(r.content, 'html.parser')
-
-    # Get relevant information from the website
-    
-    articlelist = soup.find_all('article')
-    # count = 0
-    for article in articlelist:
-        link = article.find('a', href = True)
-        if link:
-            news.append((source, link['href']))
-
-
+# Parse through BBC articles
 class BBC:
     def __init__(self, url:str):
         article = requests.get(url)
         self.soup = bs(article.content, "html.parser")
-        self.body = self.get_body()
+        self.title = self.get_title()
+
+    # Get top three articles off the homepage of the website
+    def get_top_articles(self):
+        news = []   # Create dictionary to store news articles
+        article_elements = self.soup.find_all('article', limit=3)
+        for item in article_elements:
+            for link in item.find_all('a', href = True):
+                news.append(link['href'])
+        top_titles = list(set(news))
+        return top_titles
+        
+    # Get the title of each article
+    def get_title(self) -> str:
+        title_element = self.soup.find(class_="story-body__h1")
+        if title_element is not None:
+            return title_element.text
+        else:
+            print("Title Not Found")
+
+    
+
+# Parse through Al Jazeera articles
+class AlJazeera:
+    def __init__(self, url:str):
+        article = requests.get(url)
+        self.soup = bs(article.content, "html.parser")
+        self.title = self.get_title(url)
+
+    # Get top three articles off the homepage of the website
+    def get_top_articles(self):
+        news = []
+        article_elements = self.soup.find_all('article', limit=3)
+        for item in article_elements:
+            for link in item.find_all('a', href = True):
+                news.append(link['href'])
+        top_titles = list(set(news))
+        return top_titles
+
+    # Get the title of each article
+    def get_title(self, link:str) -> str:
+        self.extracted_titles = []  # Store the extracted titles
+
+        pattern = r"/news/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<title>[\w-]+)"
+        match = re.match(pattern, link)
+            
+        if match:
+            # Extract the matched groups
+            title = match.group('title').replace('-', ' ')
+            self.extracted_titles.append(title) # Append the title to a list of extracted titles
+            return title
+    
+
+# Parse through Reuters articles
+class Reuters:
+    def __init__(self, url:str):
+        article = requests.get(url)
+        self.soup = bs(article.content, "html.parser")
         self.title = self.get_title()
         
-    def get_body(self) -> list:
-        body = self.soup.find(property="articleBody")
-        return [p.text for p in body.find_all("p")]
-    
+    # Get top three articles off the homepage of the website
+    def get_top_articles(self):
+        news = []
+        article_elements = self.soup.find_all('article', limit=3)
+        for item in article_elements:
+            for link in item.find_all('a', href = True):
+                news.append(link['href'])
+        top_titles = list(set(news))
+        return top_titles
+
+    # Get the title of each article
     def get_title(self) -> str:
-        return self.soup.find(class_="story-body__h1").text
-
-parsed = BBC(news)
-
-
-
-            # count += 1
-            # if count == 3:
-            #     break
-#     news.append(articlelist)
-#to limit to 3 articles
-# news = news[:3]
-# remove duplicates
-# news = list(set(news))
-# news = news[:12]
-
-# print(news)
-# # Put article data in a csv to check above fn works
-# df = pd.DataFrame(news)
-# df.to_csv (r'C:\Users\meimeixu@Zhimeis-MacBook-Pro\Downloads\news.csv', index = False, header = True)
-
-data = json.loads(soup.select_one("#__NEXT_DATA__").text)
-
-def extract_title(news):
-    extracted = []
-    # Define the pattern to match the date and title
-    for name, link in news:
+        title_element = self.soup.find(property="og_title")
+        if title_element is not None:
+            return title_element.text
+        else:
+            print("Title Not Found")
     
-        if name == "Al Jazeera":
-            
-            pattern = r"/news/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<title>[\w-]+)"
-            # print(link)
-            match = re.match(pattern, link)
-            
-            if match:
-                # Extract the matched groups
-                title = match.group('title').replace('-', ' ')
-                extracted.append(title)
-            # else:
-            #     return None
-    return extracted
-    # # elif "bbc.com" in link:
-    # #     pattern = r"/" # The URL doesn't contain the title
-    # # elif "reuters.com" in link:
-    # #     pattern = r"/(?P<desk>)/(?P<region>)/(?P<title>[w-]+)-2024-04-26/"
-    # # elif "scmp.com" in link:
-    # #     pattern = r"news/china/diplomacy/article/3260454/(?P<title>[w-]+)g?module=top_story&pgtype=homepage"
-    
-    # # Use regular expression to find matches
-    #         
-    
-title = extract_title(news)
-print(title)
-# def extract_title(link):
-
-#     for i in df['0']: # needs to be fixed so that we're not using df and so that 'link' works
-#         date, title = extract_date_and_title(i)
         
-#         if date and title:
-#             print("Date:", date)
-#             print("Title:", title)
-#         else:
-#             print("Invalid link format.")
+# Parse through South China Morning Post articles
+class SCMP:
+    def __init__(self, url:str):
+        article = requests.get(url)
+        self.soup = bs(article.content, "html.parser")
+        self.title = self.get_title()
 
+    # Get top three articles off the homepage of the website
+    def get_top_articles(self):
+        news = []
+        article_elements = self.soup.find_all('article', limit=3)
+        for item in article_elements:
+            for link in item.find_all('a', href = True):
+                news.append(link['href'])
+        top_titles = list(set(news))
+        return top_titles
 
+    # Get the title of each article
+    def get_title(self) -> str:
+        title_element = self.soup.select_one('span[data-qa="ContentHeadline-Headline"]')
         
-# def send_email(subject, body):
-#     from_email = "your_email@gmail.com"  # Update with your email
-#     to_email = "recipient_email@example.com"  # Update with recipient's email
-#     msg = MIMEMultipart()
-#     msg['From'] = from_email
-#     msg['To'] = to_email
-#     msg['Subject'] = subject
-
-#     msg.attach(MIMEText(body, 'plain'))
-
-#     server = smtplib.SMTP('smtp.gmail.com', 587)
-#     server.starttls()
-#     server.login(from_email, "your_password")  # Update with your password
-#     text = msg.as_string()
-#     server.sendmail(from_email, to_email, text)
-#     server.quit()
+        # Make sure the title element was found before trying to access its text
+        if title_element is not None:
+            return title_element.text
+        else:
+            print("Title Not Found")
     
+
+# Parse through AP World News articles
+class AP:
+    def __init__(self, url:str):
+        article = requests.get(url)
+        self.soup = bs(article.content, "html.parser")
+        self.title = self.get_title()
+
+    # Get top three articles off the homepage of the website
+    def get_top_articles(self):
+        news = []
+        article_elements = self.soup.find_all('article', limit=3)
+        for item in article_elements:
+            for link in item.find_all('a', href = True):
+                news.append(link['href'])
+        top_titles = list(set(news))
+        return top_titles
+
+    # Search for title in HTML code
+    def get_title(self) -> str:
+        title_element = self.soup.find('h1', class_='Page-headline')
+        if title_element is not None:
+            return title_element.text
+        else:
+            print("Title Not Found")
+
+# Create a dictionary of classes
+classes_list = [BBC, AlJazeera, Reuters, SCMP, AP]
+
+# Use zip to pair url and corresponding class
+for url, NewsWebsite in zip(URL_list, classes_list):
+    website = NewsWebsite(url)
+    top_titles = website.get_top_articles() # Call the new method
+    print(top_titles)
+
+# Put article data in a csv
+df = pd.DataFrame(top_titles)
+df.to_csv (r'C:\Users\meimeixu@Zhimeis-MacBook-Pro\Downloads\news.csv', index = False, header = True)
+
+
+# Create newsletter to send to subscriber
+def send_email(subject, body):
+    from_email = "your_email@gmail.com"  # Update with your email
+    to_email = "recipient_email@example.com"  # Update with recipient's email
+    msg = MIMEMultipart()
+    msg['From'] = from_email
+    msg['To'] = to_email
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(body, 'plain'))
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(from_email, "your_password")  # Update with your password
+    text = msg.as_string()
+    server.sendmail(from_email, to_email, text)
+    server.quit()
+
+# Compose the text of the newsletter
 def compose_email_body(df):
     body = "Today's News Headlines:\n\n Today is ['date']. Here are the top stories from around the world."
     for index, row in df.iterrows():
@@ -155,6 +209,4 @@ while True:
     time.sleep(1)
 
 
-
-    
 
